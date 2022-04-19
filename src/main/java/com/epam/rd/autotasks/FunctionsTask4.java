1ab9600c80f1403c24fc1e87b49dc1a3c802e900
mmveres <mmveres@gmail.com>
2022-04-19 15:30:18 +0300

    function-task4-master

---
+++ src/main/java/com/epam/rd/autotasks/FunctionsTask4.java
@@ -0,0 +1,15 @@
+package com.epam.rd.autotasks;
+
+public class FunctionsTask4 {
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     * if set invalid arguments in method, then method must throws
+     * IllegalArgumentException
+     */
+    public static double sumGeometricElements(int a1, double t, int alim) {
+        //TODO: Delete line below and write your own solution
+        throw new UnsupportedOperationException();
+    }
+}
